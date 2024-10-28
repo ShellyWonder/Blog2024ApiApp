cf75b49eab3921860d12b232efe2924d882baac6
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-10-28 15:09:10 -0500

    Converted HomeController to a RESTful API controller.

--- Blog2024Api/Controllers/HomeController.cs
+++ Blog2024Api/Controllers/HomeController.cs
@@ -4,64 +4,86 @@ using Blog2024ApiApp.Enums;
 using Blog2024ApiApp.Models;
 using Blog2024ApiApp.Services.Interfaces;
 using Blog2024ApiApp.DTO;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Blog2024ApiApp.Controllers
 {
-    #region PRIMARY CONSTRUCTOR
-    public class HomeController(ILogger<HomeController> logger, 
-                                IBlogEmailSender emailSender,
-                                IBlogService blogService) : Controller
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
     {
-        private readonly ILogger<HomeController> _logger = logger;
-        private readonly IBlogEmailSender _emailSender = emailSender;
-        private readonly IBlogService _blogService = blogService;
+        private readonly ILogger<HomeController> _logger;
+        private readonly IBlogEmailSender _emailSender;
+        private readonly IBlogService _blogService;
+
+    #region  CONSTRUCTOR
+        public HomeController(ILogger<HomeController> logger, 
+                                    IBlogEmailSender emailSender,
+                                    IBlogService blogService)
+        {
+            _logger = logger;
+            _emailSender = emailSender;
+            _blogService = blogService;
+        }
         #endregion
 
         #region GET INDEX/productionReady posts by blog
-        public async Task<IActionResult> Index(int? page)
+        // GET: api/Home/Index/productionReady posts by blog
+        [HttpGet("Index")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult<IEnumerable<Blog>>> Index(int? page)
         {
             var pageNumber = page ?? 1;
             var pageSize = 5;
             //Grabs all status: productionReady posts by blog in descending order by date
             var blogs = await _blogService.GetBlogsByStateAsync(PostState.ProductionReady, pageNumber, pageSize);
-            return View(blogs);
+            return Ok(blogs);
         }
         #endregion
 
         #region GET ABOUT
+        // GET: api/Home/About
+        [HttpGet("About")]
+        [ValidateAntiForgeryToken]
         public IActionResult About()
         {
-            return View();
+            return Ok();
         }
         #endregion
 
         #region GET CONTACT
+        [HttpGet("Contact")]
+        [ValidateAntiForgeryToken]
         public IActionResult Contact()
         {
-            return View();
+            return Ok();
         }
         #endregion
 
         #region POST CONTACT
-        [HttpPost]
+        [HttpPost("Contact")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Contact(ContactUs model)
+        public async Task<IActionResult> Contact([FromBody]ContactUsDTO model)
         {
-            //build email
-            model.Message = $"{model.Message}<hr>" +
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                //build email
+                model.Message = $"{model.Message}<hr>" +
             //using the Ternary (Conditional) operator (??) to check whether model.Phone is null 
             $"Phone: {(string.IsNullOrWhiteSpace(model.Phone) ? "Phone: Not Provided" : model.Phone)}";
-            await _emailSender.SendContactEmailAsync(model.Email, model.Name, model.Subject, model.Message);
-            return RedirectToAction("Index");
-        }
-        #endregion
+                await _emailSender.SendContactEmailAsync(model.Email, model.Name, model.Subject, model.Message);
 
-        #region ERROR
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                return Ok(new {message = "Your message has been sent successfully." });
+            }
+            catch (Exception)
+            {
+                return NotFound(new { message = "Your message could not be sent. Please try again." });
+            }
         }
         #endregion
+ 
     }
 }
