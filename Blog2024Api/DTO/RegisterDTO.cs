3ceb14d6c871298e099b77f14837f87c027b5bc1
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-10-29 23:15:24 -0500

    Created/Modified DTOs to implement in the Identity feature.

---
+++ Blog2024Api/DTO/RegisterDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog2024Api.DTO
+{
+    public class RegisterDTO
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
--- Blog2024Api/DTO/UserDTO.cs
+++ Blog2024Api/DTO/UserDTO.cs
@@ -3,6 +3,14 @@
     public class UserDTO
     {
         public string? Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string? FullName { get; set; }
+        public string? GitHubUrl { get; set; }
+        public string? LinkedInUrl { get; set; }
+        public byte[]? ImageData { get; set; }
+        public string? ImageType { get; set; }
     }
 }
