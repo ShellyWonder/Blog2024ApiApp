76e1a1815cc2d80101b150ae11c673c9415ccbd7
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-10-29 21:23:42 -0500

    Program.cs Identity functionality moved into a extension class.

---
+++ Blog2024Api/Extensions/ServicesConfigurationExtensions.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Blog2024ApiApp.Data.Repositories.Interfaces;
+using Blog2024ApiApp.Data.Repositories;
+using Blog2024ApiApp.Services.Interfaces;
+using Blog2024ApiApp.Services;
+using Blog2024ApiApp.DTO;
+using Blog2024ApiApp.Data.SeedData;
+using Blog2024ApiApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Google;
+
+namespace Blog2024ApiApp.Extensions
+{
+    public static class ServicesConfigurationExtensions
+    {
+        public static IServiceCollection AddCustomServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Register application services
+            services.AddScoped<IBlogRepository, BlogRepository>();
+            services.AddScoped<IBlogService, BlogService>();
+            services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
+            services.AddScoped<IApplicationUserService, ApplicationUserService>();
+            services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<IPostService, PostService>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<ICommentService, CommentService>();
+            services.AddScoped<ITagRepository, TagRepository>();
+            services.AddScoped<ITagService, TagService>();
+            
+            services.AddScoped<IBlogEmailSender, EmailSender>();
+            services.AddScoped<IImageService, ImageService>();
+            
+
+            services.AddScoped<ISlugRepository, SlugRepository>();
+            services.AddScoped<ISlugService, SlugService>();
+            services.AddScoped<ISearchRepository, SearchRepository>();
+            services.AddScoped<ISearchService, SearchService>();
+            //data seed services
+            services.AddTransient<RolesDataService>();
+            services.AddTransient<BlogsDataService>();
+            services.AddTransient<PostsDataService>();
+            //configure MailSettings
+            services.Configure<MailSettingsDTO>(configuration.GetSection("MailSettings"));
+
+            // Configure form options
+            services.Configure<FormOptions>(options =>
+            {
+                options.MultipartBodyLengthLimit = 1024 * 1024 * 10; // 10MB
+            });
+
+            return services;
+        }
+        public static IServiceCollection AddIdentityWithRoles(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                //Use for email confirmation, password reset, etc.
+                .AddDefaultTokenProviders();
+            return services;
+        }
+        public static IServiceCollection AddIdentityWithExternalProviders(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            // Add Google and GitHub Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+                // Ensure the default challenge scheme is set
+                options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+            })
+                .AddGoogle(GoogleDefaults.AuthenticationScheme, googleOptions =>
+                {
+                    var ClientId = configuration["Authentication:Google:ClientId"];
+                    var ClientSecret = configuration["Authentication:Google:ClientSecret"];
+
+                    if (string.IsNullOrEmpty(ClientId) || string.IsNullOrEmpty(ClientSecret))
+                    {
+                        throw new Exception("Google ClientId or ClientSecret is not set in the configuration.");
+                    }
+
+                    googleOptions.ClientId = ClientId;
+                    googleOptions.ClientSecret = ClientSecret;
+                })
+                .AddGitHub(githubOptions =>
+                {
+                    var ClientId = configuration["Authentication:GitHub:ClientId"];
+                    var ClientSecret = configuration["Authentication:GitHub:ClientSecret"];
+
+                    if (string.IsNullOrEmpty(ClientId) || string.IsNullOrEmpty(ClientSecret))
+                    {
+                        throw new Exception("GitHub ClientId or ClientSecret is not set in the configuration.");
+                    }
+
+                    githubOptions.ClientId = ClientId;
+                    githubOptions.ClientSecret = ClientSecret;
+                });
+            return services;
+        }
+    }
+
+}
--- Blog2024Api/Program.cs
+++ Blog2024Api/Program.cs
@@ -1,7 +1,5 @@
 using Blog2024ApiApp.Data;
 using Blog2024ApiApp.Extensions;
-using Microsoft.AspNetCore.Authentication.Google;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,49 +11,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 //Identity configuration w/Roles
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    //Use for email confirmation, password reset, etc.
-    .AddDefaultTokenProviders();
+builder.Services.AddIdentityWithRoles(builder.Configuration);
 
 // Add Google and GitHub Authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
-    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    // Ensure the default challenge scheme is set
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-})
-    .AddGoogle(GoogleDefaults.AuthenticationScheme, googleOptions =>
-    {
-        var ClientId = builder.Configuration["Authentication:Google:ClientId"];
-        var ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+builder.Services.AddIdentityWithExternalProviders(builder.Configuration);
 
-        if (string.IsNullOrEmpty(ClientId) || string.IsNullOrEmpty(ClientSecret))
-        {
-            throw new Exception("Google ClientId or ClientSecret is not set in the configuration.");
-        }
-
-        googleOptions.ClientId = ClientId;
-        googleOptions.ClientSecret = ClientSecret;
-    })
-    .AddGitHub(githubOptions =>
-    {
-        var ClientId = builder.Configuration["Authentication:GitHub:ClientId"];
-        var ClientSecret = builder.Configuration["Authentication:GitHub:ClientSecret"];
-
-        if (string.IsNullOrEmpty(ClientId) || string.IsNullOrEmpty(ClientSecret))
-        {
-            throw new Exception("GitHub ClientId or ClientSecret is not set in the configuration.");
-        }
-
-        githubOptions.ClientId = ClientId;
-        githubOptions.ClientSecret = ClientSecret;
-    });
+builder.Services.AddCustomServices(builder.Configuration);
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddControllers();
-builder.Services.AddCustomServices(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -74,7 +39,10 @@ if (app.Environment.IsDevelopment())
     builder.Configuration.AddUserSecrets<Program>();
     app.UseMigrationsEndPoint();
 }
-
+//NOTE:captures unhandled exceptions
+//and sends a consistent error response format to the client. 
+//The frontend can parse this JSON response
+//and display an error using its own view model.
 app.UseExceptionHandler(errorApp =>
 {
     errorApp.Run(async context =>
@@ -86,8 +54,6 @@ app.UseExceptionHandler(errorApp =>
         await context.Response.WriteAsJsonAsync(errorResponse);
     });
 });
-
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
